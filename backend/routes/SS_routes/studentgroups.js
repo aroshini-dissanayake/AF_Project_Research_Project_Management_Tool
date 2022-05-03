592a7f600dba44ea90f72a73de706aa28a731745
Sajani Jayasinghe <sajani@codevus.com>
2022-05-03 12:56:04 +0530

    frontend changes

--- backend/routes/SS_routes/studentgroups.js
+++ backend/routes/SS_routes/studentgroups.js
@@ -4,7 +4,7 @@ const Studentgroups = require("../../models/SS_models/studentgroups");
 const router = express.Router();
 
 // create student groups
-router.post('/studentgroups',(req, res) => {
+router.post("/studentgroups/save", (req, res) => {
   let newStudentgroups = new Studentgroups(req.body);
   newStudentgroups.save((err) => {
     if (err) {
@@ -18,92 +18,94 @@ router.post('/studentgroups',(req, res) => {
   });
 });
 
-//add panel members to student groups
-router.post("/studentgroups/:id", async (req, res) => {
-    const groupId = req.params.id
-    try {
-      const group1 = await Studentgroups.findById(groupId)
-      if (!group1) {
-        throw new Error('There is no group..!!!')
-      }
-        const panel_Member = req.body.panelMember;
-        group1.panelMember = panel_Member;   
-        await group1.save();
-      
-        return res.status(200).json({
-        success : true,
-        studentgroups:group1
-      })
-    }catch (error) {
-        res
-          .status(500)
-          .send({ status: "error", error: error.message });
-      }
-  });
-
 //get student group details
-router.route('/displaystudentgroups').get((req,res)=>{
-  Studentgroups.find().exec((err,studentgroups)=>{
-    if(err){
+router.route("/displaystudentgroups").get((req, res) => {
+  Studentgroups.find().exec((err, studentgroups) => {
+    if (err) {
       return res.status(400).json({
-        error:err,
+        error: err,
       });
     }
     return res.status(200).json({
-      success:true,
-      existingStudentgroups :studentgroups
+      success: true,
+      existingStudentgroups: studentgroups,
     });
   });
 });
 
 //get a specific student groups
-router.route('/displaystudentgroups/:id').get((req,res)=>{
+router.route("/displaystudentgroups/:id").get((req, res) => {
   let groupID = req.params.id;
-  Studentgroups.findById(groupID,(err,studentgroups)=>{
-    if(err){
-      return res.status(400).json({success:false,err});
+  Studentgroups.findById(groupID, (err, studentgroups) => {
+    if (err) {
+      return res.status(400).json({
+        success: false,
+        err,
+      });
     }
     return res.status(200).json({
-      success:true,
-      studentgroups
+      success: true,
+      studentgroups,
     });
   });
 });
 
-//update panel member
-router.route('/update/:id').put((req,res)=>{
-  Studentgroups.findByIdAndUpdate(
-      req.params.groupID,{
-          $set:req.body
-      },
-      (err,studentgroups)=>{
-          
-          if(err){
-              return res.status(400).json({error:err});
-          }
-          
-          return res.status(200).json({
-              success: "Update Successfully"
-          });
-      });
-  });
+//add panel members to student groups
+router.post("/studentgroups/:id", async (req, res) => {
+  const groupId = req.params.id;
+  try {
+    const group1 = await Studentgroups.findById(groupId);
+    if (!group1) {
+      throw new Error("There is no group..!!!");
+    }
+    const panel_Member = req.body.panelMember;
+    group1.panelMember = panel_Member;
+    await group1.save();
 
-
-//Delete panel member
-router.route('/delete/:groupID').delete((req,res)=>{
-  Studentgroups.findByIdAndRemove(req.params.groupID).exec((err,deleteStudentgroups)=>{
-      
-      if(err) return res.status(400).json({
-          message: "Delete Unsuccessfully",err
-      });
-     
-      return res.json({
-          message: "Delete Successfull",deleteStudentgroups
-      });
-  });
+    return res.status(200).json({
+      success: true,
+      studentgroups: group1,
+    });
+  } catch (error) {
+    res.status(500).send({ status: "error", error: error.message });
+  }
 });
 
+//update panel member
+router.route("/update/:groupID").put((req, res) => {
+  Studentgroups.findByIdAndUpdate(
+    req.params.groupID,
+    {
+      $set: req.body,
+    },
+    (err, studentgroups) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
 
+      return res.status(200).json({
+        success: "Update Successfully",
+      });
+    }
+  );
+});
 
+//Delete panel member
+router.route("/delete/:groupID").delete((req, res) => {
+  Studentgroups.findByIdAndRemove(req.params.groupID).exec(
+    (err, deleteStudentgroups) => {
+      if (err)
+        return res.status(400).json({
+          message: "Delete Unsuccessfully",
+          err,
+        });
+
+      return res.json({
+        message: "Delete Successfull",
+        deleteStudentgroups,
+      });
+    }
+  );
+});
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,24 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import DisplayStudentgroups from './components/SS_Components/Display_studentgroups';
 import Add_panel_member from './components/SS_Components/Add_panel_member'
 import Edit_panel_member from './components/SS_Components/Edit_panel_member';
-
-
-import Signup from './components/AA_Components/student-registration';
-import StudentLogin from './components/AA_Components/student-login';
+// import Signup from './components/AA_Components/student-registration';
+// import StudentLogin from './components/AA_Components/student-login';
 
 function App() {
   return (
    <BrowserRouter>
    <Switch>
-     <Route path = "/" component={StudentLogin}exact/>
-   
+     {/* <Route path = "/" component={<StudentLogin/>}exact/> */}
+     <Route path = "/" component ={DisplayStudentgroups} exact />
+    
      <div>
-     <Route path = "/displaystudentgroups" component ={DisplayStudentgroups} />
-     <Route path = "/addpanelmember/:id" component={Add_panel_member}/>
+     {/* <Route path = "/signup" component={Signup}/> */}
+     <Route path = "/studentgroups/:id" component={Add_panel_member}/>
      <Route path = "/edit_panelmembers/:id" component= {Edit_panel_member}/>
-    
-     <Route path = "/signup" component={Signup}/>
-    
+
      </div>  
-   </Switch>
  
-    
+    </Switch>   
    </BrowserRouter>
   );
 }
