37982bbc512c8d90c7a087c226dd2e9ed5538bd2
Sajani Jayasinghe <sajani@codevus.com>
2022-05-03 23:57:40 +0530

    staff login frontend changes

--- frontend/src/components/SS_Components/staff-login.js
+++ frontend/src/components/SS_Components/staff-login.js
@@ -45,18 +45,50 @@ export default class StaffLogin extends Component {
      };
      render() {
         return (
-            <div>
-              <form onSubmit={this.userLoginSubmit} name="form"> 
-              <label> Staff ID </label>
-              <input type="text" name="username" placeholder="Enter your Staff ID" onChange={e => this.setState({ staff_id: e.target.value })} required/> <br/><br/> 
-              <label>Password</label> 
-              <input type="password" name="password" placeholder="Enter your Password" onChange={e => this.setState({ pwd: e.target.value })} required/> 
-              <br/>       
-              <button type="submit" >Login</button>           
-         </form>   
-      <center><label >Not Registered?</label> </center>
-      <center><li><a href="/signup" >Sign Up</a></li></center>
+          <div ><br/><br/><br/><br/>
+          <div class="row d-flex align-items-center justify-content-center">
+          <div style={{width: 1100,background: "#CCCCFF",height:600}}>
+            <div class="card-body" >  <br/><br/>
+              <div class="container py-5 h-90" > 
+               <div class="row d-flex align-items-center justify-content-center h-100">              
+                 <div class="col-md-8 col-lg-7 col-xl-6"> 
+                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image"/> </div>
+              
+                      <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+                         <form onSubmit={this.userLoginSubmit} name="form"> 
+                           
+                          <div class="form-outline mb-4">  
+                          <div className="col-md-9">
+                           <i className="fa fa-lock"> &nbsp;&nbsp;</i>            
+                             <label class="form-label"><b>STAFF ID </b></label>
+                              <input type="text" name="username" class="form-control " placeholder="Enter your Staff ID" 
+                                     onChange={e => this.setState({ staff_id: e.target.value })} required/>
+                            </div>
+                          </div>
+
+                          <div class="form-outline mb-4" >
+                            <div className="col-md-9">
+                            <i className="fa fa-key"> &nbsp;&nbsp;</i>    
+                            <label class="form-label"><b>PASSWORD</b></label>
+                            <input type="password" name="password" class="form-control " placeholder="Enter your Password" 
+                                    onChange={e => this.setState({ pwd: e.target.value })} required/> 
+                           </div>
+                         </div>
+                          <button type="submit" class="btn btn-primary">
+                            <i className="fa fa-check-circle"> &nbsp;&nbsp;  Sign in &nbsp;&nbsp;</i></button>
+               
+                          <div class="divider d-flex align-items-center my-4">
+                                <center><label >Not Registered? &nbsp;&nbsp;</label>
+                                <a href="/signup" >Sign Up</a> </center>  
+                          </div>
+                    </form>   
+                </div> 
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-  )
- }
-}
+      </div>
+        )
+      }
+    }
