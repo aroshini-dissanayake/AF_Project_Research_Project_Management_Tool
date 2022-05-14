eb0164a6875566fd19eb031768d19bd7ad058b33
Aroshini88 <arodeez88@gmail.com>
2022-05-15 01:44:34 +0530

    new change

--- backend/models/IS_models/researchtopic.js
+++ backend/models/IS_models/researchtopic.js
@@ -1,25 +1,24 @@
 const mongoose = require('mongoose');
-
 const researchtopicSchema = new mongoose.Schema({
-
-    researchTopic :{
-        type: String,
-        required:true,
-    },
-
-    groupName:{
-        type: String,
-        required:true,
-    },
-
-    groupId:{
-        type: String,
-        required:true,
-    }
-});
-
+          researchTopic :{
+            type: String,
+            required:true,
+          
+           },
+         
+          groupName:{
+              type: String,
+              required:true,
+             },
+             
+          groupId:{ 
+               type: String,
+               required:true,
+        
+            }
+        });
+            
+            
 const Researchtopic = mongoose.model("researchtopic",researchtopicSchema);
 module.exports = Researchtopic;
-
-
 
