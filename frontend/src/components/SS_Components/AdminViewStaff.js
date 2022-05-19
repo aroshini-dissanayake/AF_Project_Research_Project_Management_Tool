daae528fdb9a4600d5686c3892e52df27370446a
Sajani Jayasinghe <sajani@codevus.com>
2022-05-19 08:54:17 +0530

    admin view role staff member frontend

--- frontend/src/components/SS_Components/AdminViewStaff.js
+++ frontend/src/components/SS_Components/AdminViewStaff.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import AdminNavBar from '../Layout/AdminNavBar'
+import AdminNavBar from '../Layout/AdminNavBar';
+
 
 export default class AdminViewStaff extends Component {
     constructor(props){
@@ -8,54 +9,91 @@ export default class AdminViewStaff extends Component {
         this.state = {
             staff:[]
         };
-    }   
-    componentDidMount(){
-        this.retrieveStaffRole();
     }  
 
-    retrieveStaffRole(){
-        axios.get("http://localhost:8070/admin/displayStaffRole").then(res=>{
-            if(res.data.success){
-                this.setState({
-                    staff:res.data.existingDisplayStaffRole
-        });
-     }
+//retrive all staff members 
+  componentDidMount(){
+      this.retrieveStaffRole();
+  }  
+
+  retrieveStaffRole(){
+      axios.get("http://localhost:8070/admin/displayStaffRole").then(res=>{
+          if(res.data.success){
+              this.setState({
+                  staff:res.data.existingDisplayStaffRole
+      });
+    }
   })
+}  
+
+//serch  staff members using staff id, faculty, field and role
+filterData(staff,searchKey){
+  const result = staff.filter((staff)=>
+  staff.staff_id.toLowerCase().includes(searchKey)||
+  staff.faculty.toLowerCase().includes(searchKey)||
+  staff.feild.toLowerCase().includes(searchKey) || 
+  staff.role.toLowerCase().includes(searchKey) 
+  )
+  this.setState({staff:result})
 }
+handleSearchArea = (e)=>{
+  const searchKey = e.currentTarget.value;
+    axios.get("http://localhost:8070/admin/displayStaffRole").then(res=>{
+      if(res.data.success){
+      this.filterData(res.data.existingDisplayStaffRole,searchKey)
+    }
+  });
+}
+
     render() {
-    return (
-      <div>
-         <AdminNavBar/> <br/><br/><br/><br/>
-         
-            <table class = "table" >
-              <thead>
-                  <tr bgcolor="#BCC6CC">
-                      <th scope='col'>No</th>
-                      <th scope='col'>Staff Member Name</th>
-                      <th scope='col'>Phone Number</th>
-                      <th scope='col'>Faculty</th>
-                      <th scope='col'>Feild</th>
-                      <th scope='col'>Staff ID</th>
-                      <th scope='col'>Role</th>
-                      <th scope='col'>Email</th>
-                  </tr>
-              </thead>
-              <tbody>
-                     {this.state.staff.map((staff,index)=>(
-                      <tr>
-                          <th scope='row'>{index + 1}</th>
+    return (   
+      <div >
+       <AdminNavBar/> <br/><br/>
+          <div className='container'>
+            <div className="col-md-3" >
+              <input type="text" className="form-control" style={{marginBottom:'2px'}} placeholder="Search Staff Member" onChange={this.handleSearchArea}/>
+                 <br/> 
+                    </div>               
+                      <label style={{fontSize:'18px', color:'black', fontWeight:700}}>Staff Member Role : &nbsp;&nbsp;</label>
+                     <select className="dropDown" onChange={this.handleSearchArea}>
+                   <option value="" >..</option>
+                <option value="supervisor" >Supervisor</option>
+             <option value="co-supervisor" >Co-Supervisor</option>
+          <option value="panel member" >Panel Member</option>
+      </select> 
+   <br/><br/>                          
+       <table className = "table table-hover" style={{marginTop:'30px',background: "#FFFFFF"}} >
+          <thead>
+             <tr bgcolor="#BCC6CC">
+                <th scope='col'>No</th>
+                   <th scope='col'>Staff Member Name</th>
+                     <th scope='col'>Phone Number</th>
+                       <th scope='col'>Faculty</th>
+                         <th scope='col'>Feild</th>
+                           <th scope='col'>Staff ID</th>
+                         <th scope='col'>Role</th>
+                     <th scope='col'>Email</th>
+                 </tr>
+        </thead>
+            <tbody>
+               {this.state.staff.map((staff,index)=>(
+                  <tr key={index}>    
+                      <th scope='row'>{index + 1}</th>
                           <td>{staff.name}</td>
-                          <td>{staff.phone}</td>
-                          <td>{staff.faculty}</td>
-                          <td>{staff.feild}</td>
-                          <td>{staff.staff_id}</td>
-                          <td>{staff.role}</td>
-                          <td>{staff.email}</td>
-                          </tr>
-        ))}
-          </tbody>
+                             <td>{staff.phone}</td>
+                                <td>{staff.faculty}</td>
+                                 <td>{staff.feild}</td>
+                               <td>{staff.staff_id}</td>
+                           <td>{staff.role}</td>
+                        <td>{staff.email}</td>              
+                    </tr>
+                  )
+               )}
+           </tbody>     
         </table>
-      </div>
-    )
+     </div>
+ </div>
+      )
+    }
   }
-}
+  
