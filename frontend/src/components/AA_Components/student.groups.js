209003057c65eec5e3b7ad61824a19942d336bf7
Aroshini88 <arodeez88@gmail.com>
2022-05-22 17:02:33 +0530

    group member reg frontend

--- frontend/src/components/AA_Components/student.groups.js
+++ frontend/src/components/AA_Components/student.groups.js
@@ -1,41 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Button from "@material-ui/core/Button";
+import React,{Component} from 'react' ;
+import axios from "axios";
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import AdminNavBar from '../Layout/AdminNavBar';
 
-const Groups = ({ groupId, group_name, groupMembers}) => {
-   const [groups, setGroups] = useState([])
+export default class Groups extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            studentgroups:[]
+        };
+    }
+    onReadirect(id){
+        window.location.href = `/display/${id}`
+    }
+    componentDidMount(){
+       this.retrieveStudentGroups();
+   }
+    retrieveStudentGroups(){
+        axios.get("http://localhost:8070/student/displaygroups").then(res=>{
+            if(res.data.success){
+                this.setState({
+                    studentgroups:res.data.existingGroups
+                });
+                console.log(this.state.studentgroups)
+            }
+        })
+    }
+    render(){
+        return(    
+            <div>
+                  <div className='container'>  
+                    <table class="table">
+                       <thead>
+                     <tr bgcolor="#79BAEC">
+                 <th scope='col'>No</th>
+             <th scope='col'>Group Name</th>
+         </tr>
+            </thead>
 
-   useEffect(() => {
-      const getGroupsItems = async () => {
-         try {
-            await axios.get(`http://localhost:8070/student/displaygroups`)
-               .then((res) => {
-                  setGroups(res.data.existingGroups)
-               })
-               .catch((error) => {
-                  console.log(error.message)
-               })
-         } catch (error) {
-            console.log(error.message)
-         }
-      }
-      getGroupsItems()
-   }, [])
-
-
-   return (
-      <div>
-         {/* {groups.length !== 0 ? */}
-            <div className="container">
-               <h3>Student Groups </h3>
-               <hr />
-                     {groups.map((item) => (
-                        <Button><>{item.group_name}</></Button>
-                     ))}
-            </div>
-            {/* : <div></div>} */}
-      </div>
-   )
+                <tbody>
+                {this.state.studentgroups.map((studentgroups,index) =>(
+                    <tr>
+                        <th scope='row'>{index + 1}</th>
+                        <td>{studentgroups.group_name}</td>
+                        <td>
+                        
+                             <IconButton aria-label='btn btn-success' size="small"
+                             style={{background: "#FBB917"}}
+                                onClick={()=>this.onReadirect(studentgroups._id)} >
+                             <AddCircleOutlineIcon  fontSize="small" style={{color: "black"}}/>
+                             </IconButton> 
+                             &nbsp;&nbsp;&nbsp;&nbsp;
+                            
+                             <IconButton aria-label="delete" size="small"
+                              style={{background: "#800000"}} >
+                             <DeleteForeverIcon fontSize="small"  style={{color: "white"}}/>
+                             </IconButton>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>     
+        </div> 
+        )}
 }
-
-export default Groups
