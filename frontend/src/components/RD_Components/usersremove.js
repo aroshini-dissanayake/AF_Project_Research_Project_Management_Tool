edd2be98fffb46fc2d454c854266e773995de3cc
Sajani Jayasinghe <jayasinghesajani98@gmail.com>
2022-06-04 02:31:16 +0530

    changes

--- frontend/src/components/RD_Components/usersremove.js
+++ frontend/src/components/RD_Components/usersremove.js
@@ -84,6 +84,7 @@ render() {
                            <th scope='col'>Staff ID</th>
                          <th scope='col'>Role</th>
                      <th scope='col'>Email</th>
+                     <th scope='col'>Action</th>
                  </tr>
              </thead>
                <tbody>
